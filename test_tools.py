af776b4ff7504ceafb7dbed96f45d6760915ea19
avrymi-asraf <avreymi.asraf@gmail.com>
2023-08-08 01:07:05 +0300

    add visualztion for timeline

--- test_tools.py
+++ test_tools.py
@@ -1,4 +1,5 @@
 from tools import *
+import time
 
 
 def test_show_cordinte():
@@ -8,5 +9,21 @@ def test_show_cordinte():
     show_cordinte(data).show()
 
 
+def test_slider_time_line():
+    def f(x):
+        return x + 0.1
+
+    start = time.time()
+    data = torch.rand(1000, 5)
+    data[:, 3:] = data[:, 3:] * 360
+    data[:, :3] = data[:, :3]
+    timeline = create_timeline_series(data, f, 20)
+    print(
+        f"create timeline series with simple fucnction f cost {time.time() - start} seconds"
+    )
+    slider_time_line(timeline).show()
+
+
 if __name__ == "__main__":
-    test_show_cordinte()
+    # test_show_cordinte()
+    test_slider_time_line()
--- tools.py
+++ tools.py
@@ -6,11 +6,11 @@ import plotly.figure_factory as ff
 import pandas as pd
 import timeit
 import numpy as np
-from typing import Tuple
-
+from typing import Tuple, Callable
 
 BoxDimType = Tuple[float, float]
 TensorType = torch.Tensor
+UpdateFuncType = Callable[[TensorType], TensorType]
 
 
 def spherical_to_cartesian(
@@ -27,7 +27,7 @@ def spherical_to_cartesian(
     Returns:
         TensorType: tensor (N, 3) of cartesian coordinates
     """ """"""
-    out = torch.stack(
+    out = torch.column_stack(
         [
             r * torch.sin(torch.deg2rad(theta)) * torch.cos(np.deg2rad(phi)),
             r * torch.sin(torch.deg2rad(theta)) * torch.sin(torch.deg2rad(phi)),
@@ -35,7 +35,7 @@ def spherical_to_cartesian(
         ]
     )
     if normalize:
-        return out / torch.linalg.norm(out)
+        return out / torch.linalg.norm(out, dim=1)[:, None]
     return out
 
 
@@ -64,7 +64,7 @@ def cone_by_spherical(
     z: TensorType,
     theta: TensorType,
     phi: TensorType,
-    r: float = 1.0,
+    r: TensorType = torch.ones(1),
 ) -> go.Cone:
     """return a plotly cone object by the spherical coordinates, which is a tensor of shape (N, 5) with
     the first three columns as the x, y, z coordinates, and the last two columns as the theta, phi angles.
@@ -81,8 +81,8 @@ def cone_by_spherical(
     Returns:
         go.Cone: _description_
     """
-    u, v, w = spherical_to_cartesian(r, theta, phi, normalize=False)
-    return go.Cone(x=x, y=y, z=z, u=u, v=v, w=w, anchor="tail", sizemode="absolute")
+    u, v, w = spherical_to_cartesian(r, theta, phi, normalize=False).T
+    return go.Cone(x=x, y=y, z=z, u=u, v=v, w=w, anchor="tail", sizemode="absolute",colorscale=None,)
 
 
 def show_cordinte(
@@ -118,3 +118,86 @@ def show_cordinte(
         ),
         coloraxis_showscale=False,
     )
+
+
+def slider_time_line(
+    data: TensorType, box_dim: BoxDimType = (-5, 5), nticks: int = 4
+) -> go.Figure:
+    """return a plotly figure of the data, with a slider to change the time
+
+    Args:
+        data (TensorType): data of shape (N, 5) 
+        with the first three columns as the x, y, z coordinates,
+        and the last two columns as the theta, phi angles.
+
+    Returns:
+        go.Figure: figure with slider.
+    """
+    fig = go.Figure(data=cone_by_spherical(*data[0].T))
+    steps = []
+    for i in range(1, len(data)):
+        fig.add_trace(cone_by_spherical(*data[i].T))
+        step = dict(
+            method="update",
+            args=[
+                {"visible": [False] * len(data)},
+                {"title": f"Time: {i}"},
+            ],
+        )
+        step["args"][0]["visible"][i] = True  # Toggle i'th trace to "visible"
+        steps.append(step)
+
+    sliders = [
+        dict(
+            active=0,
+            currentvalue={"prefix": "Time: "},
+            pad={"t": 50},
+            steps=steps,
+        )
+    ]
+
+    fig.update_layout(
+        sliders=sliders,
+        scene=dict(
+            aspectratio=dict(x=1, y=1, z=0.8),
+            camera_eye=dict(x=1.2, y=1.2, z=0.6),
+            xaxis=dict(
+                nticks=nticks,
+                range=box_dim,
+            ),
+            yaxis=dict(
+                nticks=nticks,
+                range=box_dim,
+            ),
+            zaxis=dict(
+                nticks=nticks,
+                range=box_dim,
+            ),
+        ),
+        coloraxis_showscale=False,
+    )
+    return fig
+
+
+def create_timeline_series(
+    data: TensorType, f: UpdateFuncType, steps: int, *arg, **kargs
+) -> TensorType:
+    """create timeline for data,
+
+    Args:
+        data (TensorType): input data, tensor (N,d)
+        f (UpdateFuncType): function to update data, function :(N,d)->(N,d)
+        steps (int): num of steps to advance
+        *arg: args for f
+        **kargs: kargs for f
+
+    Returns:
+        TensorType: tensor (steps,N,d), so ret[t] is represent the data int time t
+    """
+    if steps < 0:
+        raise ValueError("steps must be grow then 0")
+    res = torch.empty(steps + 1, *data.shape)
+    res[0] = data
+    for t in range(1, steps + 1):
+        res[t] = f(res[t - 1], *arg, **kargs)
+    return res
